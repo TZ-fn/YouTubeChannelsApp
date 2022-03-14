feab8126cdec677532873956f598a9fc202b1cea
Tomek <tomekz@vp.pl>
2022-03-14 14:49:24 +0100

    Install and configure eslint, prettier and stylelint

--- server/start.js
+++ server/start.js
@@ -5,13 +5,13 @@ const koaStatic = require('koa-static');
 const getPort = require('get-port');
 
 async function runServer() {
-    const port = await getPort({ port: 3000 });
+  const port = await getPort({ port: 3000 });
 
-    const app = new Koa();
-    app.use(koaStatic(path.join(__dirname, '..', 'static')));
-    app.listen(port);
+  const app = new Koa();
+  app.use(koaStatic(path.join(__dirname, '..', 'static')));
+  app.listen(port);
 
-    console.log(`server started at http://localhost:${port}/`);
+  console.log(`server started at http://localhost:${port}/`);
 }
 
 runServer().catch(console.error);
