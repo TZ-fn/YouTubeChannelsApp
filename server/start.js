eb464ef2dc4756b627694db9adc7ae0931cb40a2
Tomek <tomekz@vp.pl>
2022-03-14 18:29:23 +0100

    Add a /channels route to return the json data

--- server/start.js
+++ server/start.js
@@ -1,13 +1,22 @@
 const path = require('path');
-
+const channels = require('../static/channels.json');
 const Koa = require('koa');
 const koaStatic = require('koa-static');
 const getPort = require('get-port');
+const Router = require('@koa/router');
 
 async function runServer() {
   const port = await getPort({ port: 3000 });
+  const router = new Router();
+
+  function getChannels(ctx) {
+    return (ctx.body = channels);
+  }
+
+  router.get('/channels', getChannels);
 
   const app = new Koa();
+  app.use(router.routes());
   app.use(koaStatic(path.join(__dirname, '..', 'static')));
   app.listen(port);
 
