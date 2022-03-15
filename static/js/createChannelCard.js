5a5f1f12faf80051aa92538736de147a7ddb098e
Tomek <tomekz@vp.pl>
2022-03-15 15:03:42 +0100

    Modify the createChannelCard function so it creates all needed HTML elements

--- static/js/createChannelCard.js
+++ static/js/createChannelCard.js
@@ -7,9 +7,12 @@ export function createChannelCard(cardData) {
   // create card logo
   const logo = document.createElement('img');
   logo.classList.add('card__logo');
+  logo.setAttribute('alt', '');
+  logo.setAttribute('src', cardData.thumbnails.high.url);
 
   // create card title
   const title = document.createElement('p');
+  title.classList.add('card__title');
   title.append(cardData.title);
 
   // create card stats
@@ -20,12 +23,29 @@ export function createChannelCard(cardData) {
   const subs = document.createElement('li');
   const videos = document.createElement('li');
   const views = document.createElement('li');
+  [subs, videos, views].forEach((element) => element.classList.add('cards__statsElement'));
 
-  subs.textContent = `Subscribers: ${formatNumberToImperial(cardData.statistics.subscriberCount)}`;
-  videos.textContent = `Videos: ${formatNumberToImperial(cardData.statistics.videoCount)}`;
-  views.textContent = `Views: ${formatNumberToImperial(cardData.statistics.viewCount)}`;
+  subs.textContent = `Subscribers: `;
+  videos.textContent = `Videos: `;
+  views.textContent = `Views: `;
 
-  [(subs, videos, views)].forEach((element) => element.classList.add('cards__statsElement'));
+  // create spans for the stats numbers
+  const subsNumber = document.createElement('span');
+  const videosNumber = document.createElement('span');
+  const viewsNumber = document.createElement('span');
+  [subsNumber, videosNumber, viewsNumber].forEach((element) =>
+    element.classList.add('cards__statsNumber'),
+  );
+
+  // add data to the span elements
+  subsNumber.textContent = formatNumberToImperial(cardData.statistics.subscriberCount);
+  videosNumber.textContent = formatNumberToImperial(cardData.statistics.videoCount);
+  viewsNumber.textContent = formatNumberToImperial(cardData.statistics.viewCount);
+
+  // add spans to the li elements
+  subs.append(subsNumber);
+  videos.append(videosNumber);
+  views.append(viewsNumber);
 
   stats.append(subs, videos, views);
 
