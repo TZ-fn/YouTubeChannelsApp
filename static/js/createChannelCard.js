e62d9011f52fe87fa3cfa33f5d3b9ad82a5ca475
Tomek <tomekz@vp.pl>
2022-03-15 13:25:35 +0100

    Create a function to format the numbers received from the API

--- static/js/createChannelCard.js
+++ static/js/createChannelCard.js
@@ -1,10 +1,12 @@
+import { formatNumberToImperial } from './utils.js';
+
 export function createChannelCard(cardData) {
   const card = document.createElement('div');
-  card.classList += 'card-wrapper';
+  card.classList.add('card__wrapper');
 
   // create card logo
   const logo = document.createElement('img');
-  logo.classList += 'card__logo';
+  logo.classList.add('card__logo');
 
   // create card title
   const title = document.createElement('p');
@@ -12,14 +14,18 @@ export function createChannelCard(cardData) {
 
   // create card stats
   const stats = document.createElement('ul');
-  stats.classList += 'card__stats';
+  stats.classList.add('card__stats');
 
   // create card stats elements
   const subs = document.createElement('li');
   const videos = document.createElement('li');
   const views = document.createElement('li');
 
-  [subs, videos, views].forEach((element) => (element.classList += 'cards__statsElement'));
+  subs.textContent = `Subscribers: ${formatNumberToImperial(cardData.statistics.subscriberCount)}`;
+  videos.textContent = `Videos: ${formatNumberToImperial(cardData.statistics.videoCount)}`;
+  views.textContent = `Views: ${formatNumberToImperial(cardData.statistics.viewCount)}`;
+
+  [(subs, videos, views)].forEach((element) => element.classList.add('cards__statsElement'));
 
   stats.append(subs, videos, views);
 
---
+++ static/js/utils.js
@@ -0,0 +1,7 @@
+function clearNumber(number) {
+  return number.replace(/,|\.| /g, '');
+}
+
+export function formatNumberToImperial(number) {
+  return new Intl.NumberFormat('en-GB').format(clearNumber(number));
+}
